c440772d7c5f9ea5fb6d9eef36c45a9c9eef5ac7
coisincoisadojj <jonathanjj21jj@gmail.com>
2025-07-11 22:17:58 -0300

    corrigi um erro ae

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,6 +9,7 @@ router.post('/login',
   body('email').isEmail(),
   body('password').isLength({ min: 6 }),
   (req, res, next) => {
+    console.log('Requisição POST /auth/login recebida'); // <-- linha adicionada
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     next();
